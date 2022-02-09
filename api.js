94a8e965e37f19a2dbc8a528ec2c7c191798808f
Zanhour03 <83948130+Zanhour03@users.noreply.github.com>
2022-02-09 12:00:12 +0100

    Update api.js

--- api.js
+++ api.js
@@ -1,7 +1,8 @@
-
+//hämtar alla books  med hjälp av api och callar de för data
   $.get('https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=CBrtc0EUb672U4KX9biDZPWLX0dIOUAF', function(data){
+  // skapar en variabel Book som går in i datan results och hämtar alla 15 böcker
   var Books = data.results.books;
-
+//skapar en foreach loop för alla böcker i funktionen creatCard
   Books.forEach(createCard);
 
   
@@ -10,6 +11,7 @@
 
 
 function  createCard(item){
+  //skapar div, img, paragraph, button, h1 och h5 för att kunna ha bild text i en card och de blir stylade med bootstrap och css
   var div = $("<div style= 'display:inline-block; margin-left:7%; margin-bottom:2%;'></div>").attr("class", "row");
   var div0 = $("<div></div>").attr("class", "col-sm-5");
   var div1 = $("<div></div>").attr("class", "card  col-md border-5 border-dark");
@@ -20,48 +22,53 @@ function  createCard(item){
   var descrDiv =$('<p></p>').attr({ class:"card-text"}).text(item.description);
   
   var buttonDiv =$('<a>Buy</a>' ).attr({ class:"btn btn-primary", href:item.amazon_product_url});
+// end
 
-
+  
+  //lägger in img title athor button i divarana
   titleDiv.appendTo(div2);
   imgDiv.appendTo(div1);
   authorDiv.appendTo(div2);
   descrDiv.appendTo(div2);
   buttonDiv.appendTo(div2)
+//end
 
-
+  //lägger in divar in  varandra så man kan enkelt styla de och lägga de i row med hjälp av bootstrap
   div2.appendTo(div1)
   div1.appendTo(div0);
   div0.appendTo(div);
   div1.css("width", "18rem");
+  
+  //skriver ut diviarna i body så nu synns allt som finns innuti divarna på sidan
 var y = div.appendTo("body");
 
 
  
-
+//hämtar priserna för varenda bok med hjälp av böckerna isbn nummer och då kan den veta vilken bok ska ha vilket pris
   $.get(`https://booksrun.com/api/v3/price/buy/${item.primary_isbn10}?key=zux40a2q9f2a31xjk5x8`, function(dat){
-    
+    //skapar en const som hetter pris där den går in i den nya api och hämtar rätt priser
   const Pris = dat.result.offers.booksrun.new.price;
-
+// om priset inte finns i api då ska den skriva Out of stock annars så ska den skriva ut priset.
   var Price = (Pris == undefined) ? "Out of Stock" :  `${dat.result.offers.booksrun.new.price} $`;
+    //om priset inte finns(undifined) då ska knappen som säger buy inte finnas
   (Pris == undefined) ? $( buttonDiv ).remove() : false;
 
-  // if(typeof(Price) == "undefined"){
-  //    PriceStr  = "Out of stock";
 
-  // }
-  
+  //skapar en paragraph för skriva priset i
   var priceDiv = $('<p id="out" style=" margin-left:5%;"></p>').attr({ class:"card-text btn btn-primary"}).text(Price);
+    
+    //sätter in priset i en av divarna som synns på sidan så att man kan se priset
   priceDiv.appendTo(div2);
  
 
 
-  // console.log(Price);
+
 
 });
 
 
   
-// console.log(item.primary_isbn10)
+
  
   return y;
 }
